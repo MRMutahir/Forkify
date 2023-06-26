783901a5d4737d7a06b1d8537d25e63df1af0909
Mhummad Mutahir <mutahirkareem820@gmail.com>
2023-06-27 02:47:31 +0500

    second part rhe gaya hen

--- app.js
+++ app.js
@@ -1,32 +1,76 @@
 let btn_search = document.querySelector('.btn');
 let recipesection = document.querySelector('.recipe-section');
 let detailsmenu = document.querySelector('detailsmenu');
-console.log(btn_search);
+// console.log(btn_search);
+
+function aPI(search) {
+    fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${search}`)
+        .then(response => {
+            return response.json()
+        }).then(getdata => {
+            console.log(getdata, '===>> ye data API ka heen');
+            filterData(getdata);
+        });
+    // console.log(search, '===> search ji input s mili hen ');
+}
+
+function filterData(apidata) {
+    // console.log(apidata, '==>> ye api k function s aya hen');
+    let recipeData = apidata.data.recipes;
+    // console.log(recipeData, '==> ye obj s nikala tha');
+    recipeData.map(recipe => {
+            // console.log(recipe);
+            let li = document.createElement('li');
+            recipesection.appendChild(li);
+            let uiset =
+                `
+                <div style="display: flex; align-items: center;" onclick="aPI2('${recipe.id}')">
+        <img src="${recipe.image_url}" alt="" style="border-radius: 50%; width: 100px; height: 100px;">
+        <div style="margin-left: 10px; margin: 10px;">
+          <div>${recipe.title}</div>
+          <div>${recipe.publisher}</div>
+        </div>
+      </div>
+               `
+            li.innerHTML = uiset
+
+        })
+        // console.log(filtered, '==> ye map  ho kr aya hen')
+
+}
+btn_search.addEventListener('click', search)
+
+function search() {
+    let form_control = document.querySelector('.form-control');
+    let searchValue = form_control.value;
+    console.log(searchValue);
+    aPI(searchValue)
+    form_control.value = ''
+};
 
 
-fetch('https://forkify-api.herokuapp.com/api/v2/recipes?search=pizza')
-    .then(response => {
-        return response.json()
-    }).then(getdata => {
-        console.log(getdata);
-        btn_search.addEventListener("click", search)
+// after clicking on any single food item - 2;
 
-        function search() {
-            let form_control = document.querySelector('.form-control').value;
-            // console.log(getdata.data.recipes);
-            let recipesObj = getdata.data.recipes
-                // console.log(recipesObj);
-            let filtrData = recipesObj.filter((ele) => {
-                // console.log(ele.title)
-                return ele.title == 'Pizza'
-
-            })
-            console.log(filtrData)
-
-
-
-
-
-
-        }
+function aPI2(id) {
+    fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`).then(response2 => {
+        // if (!response2 == "ok") {
+        //     alert('api sahi nh hen')
+        // }
+        return response2.json();
+    }).then(singleData => {
+        console.log(singleData);
+        detailSingleItem(singleData)
     })
+};
+
+function detailSingleItem(singleitem) {
+    // console.log('hi');
+    console.log(singleitem);
+    let single = singleitem.data.recipe;
+    console.log(single)
+    let singledata = single.map(ele => {
+        console.log(ele);
+        // let singleItemUi = `<div>${ele.title}</div>`;
+        // console.log(singleItemUi);
+    })
+}
